78fcd92ae0830fce795d60294c73fb63192ad6e1
Lena Kolb <lenakolb2204@gmail.com>
2022-12-01 18:48:15 +0100

    Advent of Code Day 1

---
+++ Day1/Day1.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("assets/day1/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	var elves []int
+	var current = 0
+
+	for _, element := range lines {
+		value, err := strconv.Atoi(element)
+
+		if err != nil {
+			elves = append(elves, current)
+			current = 0
+		}
+
+		current += value
+	}
+
+	sort.Ints(elves)
+	reversed := reverse(elves)
+	fmt.Println("First: ", reversed[0])
+
+}
+
+func reverse(input []int) []int {
+	inputLen := len(input)
+	out := make([]int, inputLen)
+
+	for i, n := range input {
+		j := inputLen - i - 1
+		out[j] = n
+	}
+
+	return out
+}
