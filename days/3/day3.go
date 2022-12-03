13b748865db9243e49f6c6c944bf382cf644d774
Lena Kolb <lenakolb2204@gmail.com>
2022-12-03 17:44:37 +0100

    Add Day 3

---
+++ days/3/day3.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("/home/lenak/Dokumente/Coding/Go/AdventOfCode/assets/day3/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	var compartments [][2]string
+
+	for _, line := range lines {
+		var length = len(line)
+		var first = line[:length/2]
+		var second = line[length/2:]
+		compartments = append(compartments, [2]string{first, second})
+	}
+
+	var sameChars []string
+
+NEXT:
+	for _, element := range compartments {
+		for _, character := range element[0] {
+			for _, secondCharacter := range element[1] {
+				if character == secondCharacter {
+					sameChars = append(sameChars, string(character))
+					continue NEXT
+				}
+			}
+		}
+	}
+
+	priorities := make(map[string]int)
+
+	lowercase := "abcdefghijklmnopqrstuvwxyz"
+	uppercase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	for i, letter := range lowercase {
+		priorities[string(letter)] = i + 1
+	}
+
+	for i, letter := range uppercase {
+		priorities[string(letter)] = i + 27
+	}
+
+	var sum = 0
+
+	for _, element := range sameChars {
+		sum += priorities[element]
+	}
+
+	fmt.Println(sum)
+
+	//-------------------------------------- Task 2 --------------------------------------
+
+	var groups [][3]string
+
+	var counter = 0
+	var currentGroupIndex = 0
+	for _, line := range lines {
+		if counter == 0 {
+			groups = append(groups, [3]string{line, "", ""})
+			counter++
+		} else if counter == 1 {
+			groups[currentGroupIndex][1] = line
+			counter++
+		} else if counter == 2 {
+			groups[currentGroupIndex][2] = line
+			counter = 0
+			currentGroupIndex++
+		}
+	}
+
+	var sameGroupChars []string
+
+OUTER:
+	for _, element := range groups {
+		for _, character := range element[0] {
+			for _, character2 := range element[1] {
+				if character == character2 {
+					for _, character3 := range element[2] {
+						if character == character3 {
+							sameGroupChars = append(sameGroupChars, string(character))
+							continue OUTER
+						}
+					}
+				}
+			}
+		}
+	}
+
+	var groupSum = 0
+
+	for _, element := range sameGroupChars {
+		groupSum += priorities[element]
+	}
+
+	fmt.Println(groupSum)
+
+}
